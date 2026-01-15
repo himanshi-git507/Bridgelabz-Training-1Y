34fa195e961a97bc1927face54fff2821e78de5a
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-15 23:00:45 +0530

    ass2 prob13

---
+++ basic _programs/squareside.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SquareSide {
+    public static void main(String[] args) {
+
+        double perimeter;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the perimeter of the square: ");
+        perimeter = input.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println(
+                "The length of the side is " + side +
+                " whose perimeter is " + perimeter
+        );
+
+        input.close();
+    }
+}
