c8162f49e9a62aad39557eee1a82c31c2f70afb9
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-15 22:02:30 +0530

    Initial commit

--- basic _programs/greet.java
+++ basic _programs/greet.java
@@ -1,5 +1,5 @@
 import java.util.Scanner ;
-public class main{
+public class greet{ 
 	public static void main(String[]args){
 		Scanner sc = new Scanner (System.in);
 		System.out.println("enter the name ");
