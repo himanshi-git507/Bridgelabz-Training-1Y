2c33ab5ab59ffffaff7bcb41f89396298a09b7af
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-15 22:07:14 +0530

    Assignment prob1

---
+++ demo.java
@@ -0,0 +1,14 @@
+package com.company ;
+import java.util.Scanner ;
+public class Demo{
+	public static void main(String[]args){
+		Scanner sc = new Scanner (System.in);
+		System.out,println("enter the number");
+		int a = sc.nextInt() ;
+		int b = sc.nextInt();
+		int sum  a+b;
+		System.out.println("the sum of these numbers are :" = sum);
+	}
+}
+
+		
