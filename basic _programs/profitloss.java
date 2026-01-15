6ece54b8f4d15658ae504d06bb2e9eca311c8550
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-15 22:17:08 +0530

    ass2 prob4

---
+++ basic _programs/profitloss.java
@@ -0,0 +1,17 @@
+public class ProfitLoss {
+    public static void main(String[] args) {
+
+        double costPrice = 129;
+        double sellingPrice = 191;
+
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+
+        System.out.println(
+                "Cost Price: INR " + costPrice +
+                "\nSelling Price: INR " + sellingPrice +
+                "\nProfit: INR " + profit +
+                "\nProfit Percentage: " + profitPercentage + "%"
+        );
+    }
+}
