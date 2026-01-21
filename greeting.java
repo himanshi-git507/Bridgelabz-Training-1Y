591ee8e81767d57d16eb560a698abca636244721
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-21 21:06:46 +0530

    Truly empty workshop branch

---
+++ greeting.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner ;
+public class greeting {
+	public static void main(String[]args){
+		Scanner sc = new Scanner (System.in);
+		String name  = sc.nextLine();
+		System.out.println(" Hello " + name + " have a good day!);
+	}
+}	
+		
---
+++ main.java
@@ -0,0 +1,6 @@
+public class main{
+    public static void main(String[] args) {
+        // Print message to the console
+        System.out.println("Hello, World!");
+    }
+}
