99d76fdb022f6f69785d51dff592e4a761355f97
himanshi-git507 <himanshisoni502@gmail.com>
2026-01-26 19:52:55 +0530

    ass3que14

---
+++ basic _programs/FactorialCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FactorialCalculator  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      
+        System.out.print("Enter a positive integer: ");
+        int num = sc.nextInt();
+
+      
+        if (num < 0) {
+            System.out.println("Error: Factorial is not defined for negative numbers.");
+        } else {
+            int factorial = 1; 
+            int i = 1;         
+
+          
+            while (i <= num) {
+                factorial *= i;
+                i++;
+            }
+
+            
+            System.out.println("Factorial of " + num + " is: " + factorial);
+        }
+
+        sc.close();
+    }
+}
