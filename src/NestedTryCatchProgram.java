bfeb7db6a8c9e1caaef2ef192dfe84498442338e
himanshi-git507 <himanshisoni502@gmail.com>
2026-03-28 20:57:21 +0530

    Exception_Handling

---
+++ src/NestedTryCatchProgram.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NestedTryCatchProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int index = sc.nextInt();
+            int divisor = sc.nextInt();
+
+            try {
+                int value = arr[index];
+                try {
+                    int result = value / divisor;
+                    System.out.println(result);
+                } catch (ArithmeticException e) {
+                    System.out.println("Cannot divide by zero!");
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid array index!");
+            }
+        } catch (Exception e) {
+        }
+    }
+}
